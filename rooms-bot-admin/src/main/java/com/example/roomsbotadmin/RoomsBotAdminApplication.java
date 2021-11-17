6309c3f68d686bb1d40ce7f7bb84e0931d8d9599
Taras Bandura <56435471+Bandura1907@users.noreply.github.com>
2021-11-17 13:32:20 +0200

    enable async

--- rooms-bot-admin/src/main/java/com/example/roomsbotadmin/RoomsBotAdminApplication.java
+++ rooms-bot-admin/src/main/java/com/example/roomsbotadmin/RoomsBotAdminApplication.java
@@ -2,8 +2,10 @@ package com.example.roomsbotadmin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class RoomsBotAdminApplication {
 
     public static void main(String[] args) {
